f8c63b355c2ffbf0594193426d76c4459c11abf4
Abhijeet-96 <abhijeetchavanac31@gmai.com>
2020-10-02 12:01:26 +0530

    commit 2

--- Mobile.java
+++ Mobile.java
@@ -1,6 +1,8 @@
-class Mobile{
+import java.util.*
+;class Mobile{
     public static void main(String[] args) {
         System.out.println("hello World mobile");
+        System.out.println("hello");
     }
 
 }
